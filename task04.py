474b64037d5943c500384c7a86df2175f07ddfdc
bilal <bilalhussain42201@gmail.com>
2023-10-29 00:09:58 +0500

    create task 04 python

---
+++ task04.py
@@ -0,0 +1,28 @@
+# Lab#3 Methods in Object Oriented Programming Using Python.
+
+# Instance Method
+
+#Example 1
+class Student:
+               def __init__(self,a,b):
+                       self.a =a 
+                       self.b = b
+               def avg(self):
+                       return (self.a + self.b) /2
+
+s1 = Student(10,20)
+print(f"Average student is {s1.avg()}")
+
+
+# Example 2
+
+class Volume:
+        def calculate(self,c,d):
+                self.c = c
+                self.d = d
+                e = c * d
+                print(f"volume is {c}")
+
+z = Volume()
+z.calculate(10,20)
+
